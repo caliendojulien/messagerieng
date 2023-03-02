43a5c46ce65e89451daf715a429018cba9c8a667
Caliendo Julien <caliendo@hotmail.fr>
2023-03-02 11:05:56 +0100

    Fin du TP1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MessagesComponent } from './composants/messages/messages.component';
+import { TitreComponent } from './composants/titre/titre.component';
+import { NouveauMessageComponent } from './composants/nouveau-message/nouveau-message.component';
+import { PersonnesActivesComponent } from './composants/personnes-actives/personnes-actives.component';
+import { MessageComponent } from './composants/message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesComponent,
+    TitreComponent,
+    NouveauMessageComponent,
+    PersonnesActivesComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule
